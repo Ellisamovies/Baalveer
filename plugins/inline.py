d7b67550fa88dbf66814a2b695f5f84102c703aa
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2023-06-05 12:50:45 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -108,7 +108,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('🔎 𝖲𝖾𝖺𝗋𝖼𝗁 𝖠𝗀𝖺𝗂𝗇', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/piroxbots")
+            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/Ellisa_movies_hub")
         ]
         ]
     return InlineKeyboardMarkup(buttons)
